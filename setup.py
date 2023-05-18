e01307361a9c8bac36d4e9c61d2373c0c2a7d46a
kunal <mehtakunalg@gmail.com>
2023-05-18 05:58:06 -0500

    Data Ingestion

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 from typing import List
 
 def get_requirements(file_path:str)->List[str]:
+
     '''
     this function will return the list of requirements
 
@@ -13,6 +14,8 @@ def get_requirements(file_path:str)->List[str]:
 
         if "-e ." in requirements:
             requirements.remove("-e .")
+
+    return requirements
 
 
 setup(
